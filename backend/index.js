8520256d03c3cef904b3e78807f507bbda624616
Huzam229 <huzamkhan99@gmail.com>
2025-12-06 01:27:37 +0500

    Live Url Enabled

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,11 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // CORS
-app.use(cors());
+app.use(cors({
+  origin: "https://imago-omega.vercel.app",
+  credentials: true
+}));
+
 
 // ---------------- ROUTES ----------------
 
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api/",
+    baseURL: "https://image-generator-backend-phi.vercel.app/api/",
 })
 
 
